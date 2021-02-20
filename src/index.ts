1c546ad68638a49f76eb89afe44ced3329c96a1c
dev-song <sangsu.song@aiceltech.com>
2021-02-21 00:26:41 +0900

    Implement TypeScript interface to describe an object structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-console.log('hello');
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
 const printGreetings = (name: string, age: number, gender: string): void => {
   const greetings = `Hello, it's ${name}, age of ${age}. I'm ${gender}!`;
   console.log(greetings);
 };
 
+const printGreetingObject = (person: Human): void => {
+  const greetings = `Hello, it's ${person.name}, age of ${person.age}. I'm ${person.gender}.`;
+  console.log(greetings);
+};
+
 printGreetings('Sangsu', 30, 'male');
+printGreetingObject({ name: 'Radagast', age: 456, gender: 'male' });
