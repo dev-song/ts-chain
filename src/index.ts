e64c228e801257940bfb03db4be848881718df6f
dev-song <sangsu.song@aiceltech.com>
2021-02-21 00:33:28 +0900

    Utilize object destructuring with TypeScript interface

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,16 @@ const printGreetings = (name: string, age: number, gender: string): void => {
   const greetings = `Hello, it's ${name}, age of ${age}. I'm ${gender}!`;
   console.log(greetings);
 };
+printGreetings('Sangsu', 30, 'male');
 
 const printGreetingObject = (person: Human): void => {
   const greetings = `Hello, it's ${person.name}, age of ${person.age}. I'm ${person.gender}.`;
   console.log(greetings);
 };
+const radagast = { name: 'Radagast', age: 456, gender: 'male' };
+printGreetingObject(radagast);
 
-printGreetings('Sangsu', 30, 'male');
-printGreetingObject({ name: 'Radagast', age: 456, gender: 'male' });
+const getNameAge = ({ name, age }: Human): void => {
+  console.log(name, age);
+};
+getNameAge(radagast);
