4eb9a30040e48c0bd232481fd21fb2d0544f7633
dev-song <sangsu.song@aiceltech.com>
2021-02-21 18:37:54 +0900

    Create a class to replace TypeScript interface

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,31 @@ interface Human {
   gender: string;
 }
 
+class NeoHuman {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+const gandalf = new NeoHuman('Gandalf', 2001, 'male');
+
 const printGreetings = (name: string, age: number, gender: string): void => {
   const greetings = `Hello, it's ${name}, age of ${age}. I'm ${gender}!`;
   console.log(greetings);
 };
 printGreetings('Sangsu', 30, 'male');
 
-const printGreetingObject = (person: Human): void => {
+const printGreetingObject = (person: NeoHuman): void => {
   const greetings = `Hello, it's ${person.name}, age of ${person.age}. I'm ${person.gender}.`;
   console.log(greetings);
 };
 const radagast = { name: 'Radagast', age: 456, gender: 'male' };
-printGreetingObject(radagast);
+printGreetingObject(gandalf);
 
 const getNameAge = ({ name, age }: Human): void => {
   console.log(name, age);
