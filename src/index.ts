ea51bb8ffbe81b13ecd9c294ddccc30e79d78b08
dev-song <sangsu.song@aiceltech.com>
2021-02-21 18:59:03 +0900

    Add basic methods for accessing blockchain data
    - Use static methods in JavaScript class (can be used even though the class is not yet created)
    - crypto-js module is installed to calculate hash

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
@@ -16,5 +21,7 @@ class Block {
 
 const genesisBlock: Block = new Block(0, 'abcdefg', '', 'Hello world', 123456);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
